ad208b862a085f097a98c147b8372ee3064e5a15
david942j <david942j@users.noreply.github.com>
2017-09-11 04:13:27 -0500

    Add files via upload

---
+++ twctf-2017/steganographer_revenge/README.md
@@ -0,0 +1,1 @@
+See my [blog](http://david942j.blogspot.com/2017/09/write-up-tokyo-westerns-ctf-2017-rev500.html) for details!
--- twctf-2017/steganographer_revenge/decode.rb
+++ twctf-2017/steganographer_revenge/decode.rb
@@ -3,23 +3,21 @@
 
 N = 512
 file = 'dump_y'
-ary = IO.binread(file).split.map(&:to_f)
+ystar = IO.binread(file).split.map(&:to_f)
+e = []
+N.times do |i|
+  N.times do |j|
+    e << ystar[i * N + j] if i + j >= N / 2
+  end
+end
 280.upto(280) do |len|
   value = IO.binread("values/#{format('%02d', len)}.dump").split.map(&:to_i)
-
-  need = []
-  N.times do |i|
-    N.times do |j|
-      need << ary[i * N + j] if i + j >= N / 2
-    end
-  end
-  # p value
-  ar = []
+  result = ''
   0.upto(8 * len - 1) do |v|
     sym = [0] * v + value.dup + [0] * (len * 8 - 1 - v)
-    ar << (sym.zip(need[0, sym.size]).reduce(0.0) { |s, (a, b)| s + a * b } < 0 ? '0' : '1')
+    result << (sym.zip(e[0, sym.size]).reduce(0.0) { |s, (a, b)| s + a * b } < 0 ? '0' : '1')
   end
-  p ar.each_slice(8).map { |v| v.join.to_i(2).chr }.join
+  p result.scan(/.{8}/).map { |v| v.to_i(2) }.pack("C*")
 end
 
 # Dear CTFers, We are holding Tokyo Westerns CTF 3rd 2017 from September 2nd to September 4th. This is a security competition hosted by Tokyo Westerns. We would like to invite you to this CTF. P.S. This is my present for you. TWCTF{Spr34d_Sp3ctrum_1s_m0r3_u53fu1_f0r_st3g4n0gr4phy}.
